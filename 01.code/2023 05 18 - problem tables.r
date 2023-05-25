0c0330ab6f118db1db901c2d6b04bd5d4db69659
luiscarlosguevara <lguevara@dnp.gov.co>
2023-05-25 17:26:14 +0100

    2023 05 25 - 01 - Updated route variable list for priority table.

--- 01.code/2023 05 18 - problem tables.r
+++ 01.code/2023 05 18 - problem tables.r
@@ -46,8 +46,15 @@ dt_p <- usethis::create_download_url(gdrive_shared) |>
   tibble::as_tibble()
 rm(gdrive_shared)
 
+# Sets simplified key
+dt_p$keyp <- paste0( dt_p$DIRECTORIO,
+                     dt_p$SECUENCIA_P,
+                     dt_p$ORDEN)
+
+table(duplicated(dt_p$keyp))
+
 # Loads problem tables - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
- names(cap_list); rm(files_f())
+ names(cap_list)
  
  pdcd_largo_p <- cap_list[['pdcd_largo_p.dta']]
  pdcd_largo_sinp <- cap_list[['pdcd_largo_sinp.dta']]
@@ -57,7 +64,6 @@ rm(gdrive_shared)
  
  ## 01.1 Priority long cycle -----------------------------------------------------------------------
  pdcd_largo_p$keyp <- paste0( pdcd_largo_p$DIRECTORIO,
-                              pdcd_largo_p$SECUENCIA_ENCUESTA,
                               pdcd_largo_p$SECUENCIA_P,
                               pdcd_largo_p$ORDEN )
  
@@ -74,7 +80,6 @@ rm(gdrive_shared)
  #  
   table(!duplicated(pdcd_largo_p$keyp))
   table(pdcd_largo_p$keyp %in% dt_p$keyp) 
- 
   table(pdcd_largo_p$P3013)
   
   # Problem type reshape (P1670S Pcat A1_ Ptype) - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -103,7 +108,7 @@ rm(gdrive_shared)
   dt_ln1 <-   dt_ln1[which(dt_ln1$nj_count1 != 0),]
 
 # Problem month reshape (P3009 S1_ PCat_Ptype ) - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  #
+#
   length(grep('P3009S1_', names(pdcd_largo_p)))
   var_m <- names(pdcd_largo_p)[grep('P3009S1_', names(pdcd_largo_p))]
   
@@ -178,17 +183,16 @@ rm(gdrive_shared)
   
   temp_dt <- list(dt_mn1,dt_ye1,dt_im1,dt_ln1)
   names(temp_dt) <- c('dt_mn1','dt_ye1','dt_im1','dt_ln1')
-  
-  rm(dt_mn1,dt_ye1,dt_im1,dt_ln1);rm(dt_ln1labs,dt_im1labs,dt_mn1labs,dt_ye1labs);rm(var_lab)
-  rm(var_i,var_m,var_t,var_t,var_y)
-  
+
   temp_dt <- lapply(temp_dt, function(x){cbind(x, keypp = paste0(x[,'keyp'], x[,'type_id']))})
 
 ### 01.1.2 individual-problem frame ----------------------------------------------------------------
   # Creates single problem-individual frame, merging type, impact and date
   #
+  lapply(temp_dt, function(x){table(x[,'keypp'] %in% dt_ln1$keypp)}) # all keypp present in dt_ln1
   
-  lapply(temp_dt, function(x){table(x[,'keypp'] %in% dt_pj$keypp)}) # all keypp present in dt_pj
+  rm(dt_mn1,dt_ye1,dt_im1,dt_ln1);rm(dt_ln1labs,dt_im1labs,dt_mn1labs,dt_ye1labs);rm(var_lab)
+  rm(var_i,var_m,var_t,var_t,var_y)
   
   temp_dt[['dt_im1']]$keyp <- NULL
   temp_dt[['dt_ye1']]$keyp <- NULL
@@ -232,7 +236,6 @@ rm(gdrive_shared)
   dt_pj$nj_prio.x <- NULL
   dt_pj$nj_prio.y <- NULL
   
-  
   # At this stage the ** nj_prio ** variable only holds records from declarants with three or more
   # problems. By construction, all problems declared by individualas with as much as 2 problems
   # are considered to hold priority status. Thus, it is needed to either merge the problem ID from 
@@ -248,54 +251,164 @@ rm(gdrive_shared)
   
 # 02. PROBLEM ROUTES -------------------------------------------------------------------------------
   
-  ## 02.1 Priority long cycle ----------------------------------------------------------------------
-  ## 
-  
+  ## 02.1. Institutional route ---------------------------------------------------------------------
+  ### 02.1.1. Priority long cycle ------------------------------------------------------------------
+  #
   length(grep('A1_', names(pdcd_largo_p)))
   var_t  <- names(pdcd_largo_p)[grep('A1_', names(pdcd_largo_p))] #typologies
   var_r  <- names(pdcd_largo_p)[grep('P1672_', names(pdcd_largo_p))] #routes
   
-  # institutional route
-  var_i1 <- names(pdcd_largo_p)[grep('P1673S', names(pdcd_largo_p))] #institutions all reached
-  var_i2 <- names(pdcd_largo_p)[grep('P1674_', names(pdcd_largo_p))] #institutions last reached
+  #### 02.1.1.1 route table -------------------------------------------------------------------------
+  #
+  dt_route1 <- pdcd_largo_p[is.na(pdcd_largo_p$P3013) == FALSE, 
+                            c('keyp','keyppp1','P3013','P1672_1')]
+  
+  dt_route2 <- pdcd_largo_p[is.na(pdcd_largo_p$P3014) == FALSE, 
+                            c('keyp','keyppp2','P3014','P1672_2')]
+  
+  names(dt_route1)[names(dt_route1) == 'keyppp1'] <- 'keypp'
+  names(dt_route1)[names(dt_route1) == 'P3013']   <- 'type_id'
+  names(dt_route1)[names(dt_route1) == 'P1672_1']   <- 'P1672'
+  names(dt_route2) <-  names(dt_route1)
+
+  dt_route <- rbind(dt_route1,dt_route2);rm(dt_route1,dt_route2)
+  table(is.na(dt_route$P1672)); table((dt_route$P1672))
+  
+
+  var_i11 <- names(pdcd_largo_p)[grep('P1673S', names(pdcd_largo_p))[1:41]]  #institutions all visited
+  var_i12 <- names(pdcd_largo_p)[grep('P1673S', names(pdcd_largo_p))[42:82]] #institutions all visited
+  var_i2 <- names(pdcd_largo_p)[grep('P1674_', names(pdcd_largo_p))] #institutions last visited
   
   var_ir <- names(pdcd_largo_p)[grep('P1675_', names(pdcd_largo_p))] #institutional route reason
-  var_ir <- names(pdcd_largo_p)[grep('P1676', names(pdcd_largo_p))] #institutional route results
+  var_irr <- names(pdcd_largo_p)[grep('P1676', names(pdcd_largo_p))] #institutional route results
   
-  var_ir <- names(pdcd_largo_p)[grep('P1677', names(pdcd_largo_p))] #resons for no result
-  var_ir <- names(pdcd_largo_p)[grep('P1678', names(pdcd_largo_p))] #reasons for not continuing
+  var_irnr <- names(pdcd_largo_p)[grep('P1677', names(pdcd_largo_p))] #resons for no result
+  var_irnc <- names(pdcd_largo_p)[grep('P1678', names(pdcd_largo_p))] #reasons for not continuing
   
   # institutions, alongside types, are the only wide strcuture in the table. Thus, it is needed
   # to set the addecuated long format for thos problems properly set by priority order 2 
-  # (i.e. declaraction >= 3). There is a total of 703 of such records (linking only to problems
-  # faced using the institutional rout).
+  # (i.e. declaraction >= 3). There is a total of 1,249 of such records (linking only to problems
+  # faced using the institutional rout) under priority order 2.
   #
-  dt_inst_routPLC1 <- tidyr::gather(pdcd_largo_p[c('keyp','keyppp1','FEX_C',
-                                                   var_i1)], 
-                          institution_all, 
+  # Institutions visited (** institution_allq ** ) for priority one
+  # 
+  dt_inst_routPLC1 <- tidyr::gather(pdcd_largo_p[is.na(pdcd_largo_p$P3013) == FALSE,
+                                                 c('keyp','keyppp1','P3013',var_i11)], 
+                          institution_allq, 
                           nj_count1, 
-                          var_i1, 
+                          var_i11, 
                           factor_key = TRUE)
   
-  dt_inst_routPLC1 <-   dt_inst_routPLC1[which(dt_inst_routPLC1$nj_count1 != 0),]
+  dt_inst_routPLC1 <-   dt_inst_routPLC1[is.na(dt_inst_routPLC1$nj_count1) == FALSE,]
   
-  dt_inst_routPLC2 <- tidyr::gather(pdcd_largo_p[c('keyp','keyppp2','FEX_C',
-                                                   var_i1)], 
-                                    institution_all, 
-                                    nj_count1, 
-                                    var_i1, 
-                                    factor_key = TRUE)
+  table(duplicated(dt_inst_routPLC1$keyppp1))
   
-  dt_inst_routPLC2 <-   dt_inst_routPLC2[which(dt_inst_routPLC2$nj_count1 != 0),]
-  
-  # Rbind long institution table for each priority problem
+  # Institutions visited (** institution_allq ** ) for priority two
   # 
+  dt_inst_routPLC2 <- tidyr::gather(pdcd_largo_p[is.na(pdcd_largo_p$P3014) == FALSE,
+                                                 c('keyp','keyppp2','P3014',var_i12)], 
+                                    institution_allq, 
+                                    nj_count1, 
+                                    var_i12, 
+                                    factor_key = TRUE)
+
+  dt_inst_routPLC2 <-   dt_inst_routPLC2[is.na(dt_inst_routPLC2$nj_count1) == FALSE,]
+  table(duplicated(dt_inst_routPLC2$keyppp2))
+  
+  # Rbind long institution table for each priority problem.
+  # 
+  names(dt_inst_routPLC2)[names(dt_inst_routPLC2) == 'keyppp2'] <- 'keypp'
+  names(dt_inst_routPLC2)[names(dt_inst_routPLC2) == 'P3014']   <- 'type_id'
+  
   names(dt_inst_routPLC1) <- names(dt_inst_routPLC2)
-  dt_inst_routPLC <- rbind(dt_inst_routPLC1,dt_inst_routPLC2); rm(dt_inst_routPLC1,dt_inst_routPLC2)
+  dt_inst_routPLC <- rbind(dt_inst_routPLC1,dt_inst_routPLC2);rm(dt_inst_routPLC1,dt_inst_routPLC2)
+  
+  dt_inst_routPLC$institution_allq <- as.character(dt_inst_routPLC$institution_allq)
+  dt_inst_routPLC$institution_allq <- substr(dt_inst_routPLC$institution_allq,1,
+                                            nchar(dt_inst_routPLC$institution_allq)-2)
+  
+  dt_instlabs <- openxlsx::read.xlsx(sprintf(pat, "1tB7MQUDs4TIgB7Wgoq18QlqiJ_xUaTjd"))
+  dt_inst_routPLC <- merge(dt_inst_routPLC, dt_instlabs, all.x = TRUE, by = 'institution_allq')
+  
+  rm(var_i11,var_i12)
+  
+  ### 02.1.2. Priority long cycle ------------------------------------------------------------------
+  
+  sum(pdcd_largo_sinp$FEX_C)
+  sum(dt_pj$FEX_C[dt_pj$nj_count1 < 3])
+  
+  # Possible issue with characterization? not all problems reported in the general declaration table
+  # are recorded in the long cicle with no priority (4915632 vs 4931342). Yet all individual-problem
+  # records in the long cycle no priority should be part of the declaration table.
+  # 
+  pdcd_largo_sinp$type_id <- pdcd_largo_sinp$P3154
+  table((pdcd_largo_sinp$type_id)%in%(dt_pj$type_id))
+  
+  # In the long cycle with no priority the variables  SECUENCIA_ENCUESTA and ORDEN are exactly the
+  # same, there is a need to find out the correct intendifyer.
+  # 
+  table(pdcd_largo_p$SECUENCIA_ENCUESTA)
+  table(pdcd_largo_sinp$SECUENCIA_ENCUESTA)
+  
+  table(pdcd_largo_p$ORDEN)
+  table(pdcd_largo_sinp$ORDEN)
+  
+  # Turns out, there is a faulty labaling in the variable labeling of the table **pdcd_largo_sinp**.
+  # This  issue was making the unique identityer hard to get by. The solution forced us to 
+  # simplify the individual key in the ** dt_p ** and ** dt_pj** tables, affecting also the 
+  # problem-individual key.The varaible label correspondeces in table  ** pdcd_largo_sinp ** are as
+  # follows
+  # 
+  # ___pdcd_largo_sinp____|__pdcd_largo_p____
+  #    DIRECTORIO         |   DIRECTORIO     
+  #    HOG                |   SECUENCIA_P    
+  #    SECUENCIA_P        |   ORDEN          
+  #
+
+  # pdcd_largo_sinp$keyp <- paste0(pdcd_largo_sinp$DIRECTORIO,
+  #                                pdcd_largo_sinp$HOG,
+  #                                pdcd_largo_sinp$SECUENCIA_P)
+  
+  table(duplicated(pdcd_largo_sinp$keyp))
+  
+  pdcd_largo_sinp$keyp <- paste0(pdcd_largo_sinp$DIRECTORIO,
+                                 pdcd_largo_sinp$HOG,
+                                 pdcd_largo_sinp$SECUENCIA_P) 
+  
+  # 12.042 total problems in the long cycle no priority (declaration < 3)
+  table(pdcd_largo_sinp$keyp %in% dt_pj$keyp)
+  table(dt_pj$keyp %in% pdcd_largo_sinp$keyp)
+  table(dt_pj$nj_count1)
+  
+  pdcd_largo_sinp$keypp <- paste0(pdcd_largo_sinp$keyp, pdcd_largo_sinp$type_id)
+  table(pdcd_largo_sinp$keypp %in% dt_pj$keypp)
   
   
+  #### 02.1.1.1 route table -------------------------------------------------------------------------
   
+  table(pdcd_largo_sinp$P1672)
+  dt_route_sinp <- pdcd_largo_sinp[c('keypp',
+                                     'P1672',   # route
+                                     'P1674',   # last institution
+                                     'P1675',   # institution reason
+                                     'P1676',   # institution result
+                                     'P1676S2', # institution months
+                                     'P1676S3', # institution satisfaction
+                                     'P1677',   # instiution no result reason
+                                     'P1678',   # institution drop out reason
+                                     'P1679',   # autocomposition reason
+                                     'P1680',   # autocomposition result
+                                     'P1681',   # violence reason
+                                     'P1682',   # Illegal actor reason
+                                     'P1683',   # Inaction reason
+                                     'P1684',   # Was the agreement met?
+                                     'P1685',   # Was the problem solved
+                                     'P1686',   # Time for solution
+                                     'P1687',   # Path retaking
+                                     'P1688',   # Reasons for not reataking
+                                     'P1689',   # Legal assistance
+                                     'P1690'   # Reasons for not legal assistance
+                                     )]
   ## 02.2 No Priority long cycle -------------------------------------------------------------------
   ## 02.3 Short cycle ------------------------------------------------------------------------------
-  
   
