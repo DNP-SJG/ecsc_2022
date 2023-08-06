0de72ef8103adc8d7bc44675ff23a2cffcab03d2
luiscarlosguevara <lguevara@dnp.gov.co>
2023-08-06 18:16:52 +0100

    2023 08 06 - 01 - finishes control identification. Merges relvant variables from the 6 problem tables to the general population table.

--- 01.code/2023 07 31 - incidencia de problemas justiciables.R
+++ 01.code/2023 07 31 - incidencia de problemas justiciables.R
@@ -23,13 +23,13 @@ library(stringr) # leading zeros and other string work
 # Individuals table
 #
 gd <- c(
-  # No label tables
+  # No labeled tables
   "https://drive.google.com/file/d/1kiSAB_gY-4mXOt1EcI2x4sDTB-FG6kxl/view", #Individuals
   "https://drive.google.com/file/d/1JxTGqtd-oSfQIkbdfoQrzmMn0aMcewAj/view", #Problems
   "https://drive.google.com/file/d/1E6BV8_8cZm1nNUQbxg6IvGulYBbqcB3u/view", #Institutions
-  # Label tables
-  "https://drive.google.com/file/d/1OWQYfSHaw1jkxljqXSQExOIhbUJgiwtv/view",  #Problems
-  "https://drive.google.com/file/d/197V-R5Dkma-HDQzoCrOSXmY4aMYXrLjO/view"   #Individuals problems 
+  # Labeled tables
+  "https://drive.google.com/file/d/1OWQYfSHaw1jkxljqXSQExOIhbUJgiwtv/view", #Problems
+  "https://drive.google.com/file/d/197V-R5Dkma-HDQzoCrOSXmY4aMYXrLjO/view" #Individuals problems 
   )
 
 files_f <- function(x){
@@ -45,8 +45,51 @@ names(dt_list) <- c('dt_p','dt_pj_rt_p','dt_allvisited_inst','dt_pj_rt_pl','dt_p
 dt_pl <- as.data.frame(dt_list[['dt_pl']])
 dt_pj_rt_pl <- dt_list[['dt_pj_rt_pl']]
 
-# 00.02 DEFINES RELEVANT GROUPING VARIABLES --------------------------------------------------------
+# loads each crime sub chapter to build the incident reporting variable
 #
+pat <- "https://docs.google.com/uc?id=%s&export=download"
+files <- openxlsx::read.xlsx(sprintf(pat, "1P4x_BW5PZz8HJP5KfznhVV7vWZ_FEqKd"))
+cap_lab <- as.vector(files$cap)
+
+# loading function
+#
+files_f <- function(x){haven::read_dta(sprintf(pat,x)) }
+cap_list <- lapply(files$id_cap,files_f)
+names(cap_list) <- files$cap
+
+crimenames <-  c('ext.dta',
+'hurto_ganado.dta',
+'hurto_personas.dta',
+'hurto_residencia.dta',
+'hurto_vehiculos.dta',
+'rinas_peleas.dta')
+
+dt_pl$keyc <- paste0(dt_pl$DIRECTORIO,
+                     dt_pl$SECUENCIA_ENCUESTA,
+                     dt_pl$SECUENCIA_P,
+                     dt_pl$ORDEN)
+
+for (i in 1:length(crimenames)){
+  crime <- crimenames[i]
+  cap_list[[crime]]$keyc <- paste0(
+    cap_list[[crime]]$DIRECTORIO,
+    cap_list[[crime]]$SECUENCIA_ENCUESTA,
+    cap_list[[crime]]$SECUENCIA_P,
+    cap_list[[crime]]$ORDEN
+  )
+  print(table(duplicated(cap_list[[crime]]$key)))
+  print(table(cap_list[[crime]]$key %in% dt_pl$keyc ))
+}
+rm(crimenames,crime)
+
+dt_pl <- merge(dt_pl, cap_list[['ext.dta']][c('keyc','P1187','P1106')], all.x = TRUE)
+dt_pl <- merge(dt_pl, cap_list[['hurto_ganado.dta']][c('keyc','P2073','P2076')], all.x = TRUE)
+dt_pl <- merge(dt_pl, cap_list[['hurto_personas.dta']][c('keyc','P1324','P1113')], all.x = TRUE)
+dt_pl <- merge(dt_pl, cap_list[['hurto_residencia.dta']][c('keyc','P1228','P1214')], all.x = TRUE)
+dt_pl <- merge(dt_pl, cap_list[['hurto_vehiculos.dta']][c('keyc','P1110','P1238')], all.x = TRUE)
+dt_pl <- merge(dt_pl, cap_list[['rinas_peleas.dta']][c('keyc','P1294','P1227')], all.x = TRUE)
+
+rm(i,pat,cap_lab,cap_list,files_f)
 
 ## Justiciable problem count and id ----------------------------------------------------------------
 ## 
@@ -66,7 +109,116 @@ dt_pl$a18 <- 0
 dt_pl$a18[dt_pl$P5785 > 17] <- 1
 table(dt_pl$a18, dt_pl$jp)
 
-#dt_pl <- dt_pl[dt_pl$P5785 > 14,]
+dt_pl <- dt_pl[dt_pl$P5785 > 14,]
+
+# 00.02 DEFINES RELEVANT GROUPING VARIABLES --------------------------------------------------------
+#
+
+# Demographics - - - -
+# P3038 romantic attraction
+# P220 sex
+# P3039 gender
+# P5785 age
+# Clase household type
+# P1988 electricity in the household
+# P1988S1 household strata
+# P1989 household ownership
+# P3303 internet access/connection in the last year
+# P6080 self recognition
+# P6210 highest educational level achieved
+# P6210S1 education degree
+# P1366 Marital status
+# P756 Birth place
+
+# Disabilities - - - - 
+# P1906S1 hearing
+# P1906S2 speaking
+# P1906S3 seeing
+# P1906S4 moving
+# P1906S5 handling objects
+# P1906S6 memory and decisions
+# P1906S7 self maintenance
+# P1906S8 social interactions
+# P1906S9 respiratory and heart limitations
+
+# Socio economic - - - -
+# 
+# P1365 activities (work)
+# P3105 security perception (local)
+# P3106 security perception when walking alone at night
+# P3107 security perception (municipality/city)
+# P564 prospects on being a victim in the future
+# P3503S1 SWB life
+# P3503S2 SWB health
+# P3503S3 SWB economic outlook
+# P3503S4 SWB work
+# P3503S5 SWB emotional outlook
+# P3503S6 SWB relationships
+
+# Street abuse and sexual abuse experiences - - - -
+# 
+# P3302S1 street harassment of sexual nature
+# P3302S2 body shame, sexual preferences shame
+# P3302S3 sexual intentions
+# P3302S4 non-consented kissing/hugging
+# P3302S5 sexual proposal/ force dating
+# P3302S6 unwanted sexual gifts
+# P3302S7 on-line sexual harassment
+# P3302S8 sexual public exhibition
+# P1976S13 street sexual harassment 
+# P1976S14 sexual violence or aggression
+
+# Crimes and victimization - - - -
+#
+# P541 household theft 2022
+# P523 vehicle theft
+# P525 personal object theft
+# P526 physical aggression an quarrels 
+# P528 extortion
+# P1959 animal theft
+# P195 other
+# P1392 household theft 2021
+# P1960 animal theft
+# P1179 vehicle theft
+# P1343 personal object theft
+# P1315 physical aggression an quarrels 
+# P1286 extortion
+# P1976S1 other
+
+# Physical aggression - - - -
+# 
+# P3115S1 push
+# P3115S2 hit
+# P3115S3 object/weapon
+# P3115S4 other
+
+# Crime reporting - - - -
+#
+# P1228 household theft
+# P1214 household theft reasons to avoid reporting
+# P2073 animal theft
+# P2076 animal theft reasons to avoid reporting
+# P1238 vehicle theft
+# P1110 vehicle theft reasons to avoid reporting
+# P1324 personal object theft
+# P1113 personal object theft reasons to avoid reporting
+# P1294 physical aggression an quarrels 
+# P1227 physical aggression an quarrels reasons to avoid reporting
+# P1187 extortion
+# P1106 extortion reasons to avoid reporting
+
+# Contribution to security - - - - 
+# 
+# P1182S1 Police
+# P1182S2 Military
+# P1182S3 Major
+# P1181S1 Prosecutor office
+# P1181S2 Judges
+# P3317S1 ICBF
+# P3317S2 Police inspections
+# P3317S3 Conciliation centres
+
+
 
 ## Education ---------------------------------------------------------------------------------------
 ##
@@ -80,11 +232,6 @@ dt_pl$edug <- factor(dt_pl$edug)
 table(dt_pl$vic); table(is.na(dt_pl$vic))
 dt_pl$vic[is.na(dt_pl$vic)] <- 0 # As the Vic variable was build under the declaration table
 # all individuals who experienced a Vic circumstance  also declared JP. This is a problem.
-
-# Household Theft - - - - - - -
-# 
-# 2022 
-# 
 
 tonumber <- function(x){as.numeric(dt_pl[,x])}
 vicvars <- c('P541', 'P523','P525','P526','P528','P1959','P1956',#2022
@@ -176,6 +323,12 @@ p <- ggplot(dt_pl[dt_pl[,x] != 0 & dt_pl[,x] != 9,],
 print(p)
 rm(p)
 }
+
+## Exposure to physical violence  ------------------------------------------------------------------
+## 
+
+
+
 
 # 01. CLASS, AGE MARGIN, SEX, DECLARATION ----------------------------------------------------------
 # Sandkey
